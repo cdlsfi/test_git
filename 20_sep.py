07084ca651e328dd4cb258d125d58188f0ba4f02
yang <h@in.be>
2022-09-23 14:22:59 +0200

    first commit

---
+++ 20_sep.py
@@ -0,0 +1,12 @@
+# mot = "banana"
+# ok = False
+# for index in range(len(mot)):
+#     for odeur in range(index+1,len[mot]):
+#         if mot[index]== mot[odeur]:
+#             ok= True
+
+
+
+words = ["I", "really", "love", "Python"]
+print("Python" in words)
+print("I" in words)
