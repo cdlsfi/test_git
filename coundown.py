bb484c30e01dd22ca9afbcc02e7a3b8c72f81a4b
yang <h@in.be>
2022-09-23 14:31:40 +0200

    fdgf

---
+++ coundown.py
@@ -0,0 +1,6 @@
+a = 11
+
+while a > 0 :
+    
+    a = a -1 
+    print(a)
---
+++ prof.py
@@ -0,0 +1,10 @@
+from random import randint
+
+names = ["iran", "der", "slkh", "gjh", "kdhgp","kjdsnbc"]
+
+index = randint(0,len(names) - 1)
+
+print(names)
+
+print(names[index])
+
