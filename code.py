907f9b19973e55922179861613a4853ea2af1345
yang <h@in.be>
2022-09-23 14:19:15 +0200

    j'ajoute dans le lis

---
+++ code.py
@@ -0,0 +1,7 @@
+code = input("Entrer votre mot de passe : ")
+if code != "Pyth0n" :
+    print("Code erroné.")
+    if code == "Python" :
+        print("Noubliez pas de mettre le 0")
+else: 
+    print("code valide!")   
---
+++ listes.py
@@ -0,0 +1,16 @@
+lettres = [ "a", "b", "c", "d", "e", "f"]
+print(lettres)
+
+from random import randint
+list_de_code = []
+
+while len(list_de_code)  < 4 :
+
+    index = randint(0,5)
+    list_de_code.append(lettres[index])
+print(list_de_code)
+
+
+
+
+j'ajoute quelque chose sur lisytes.py
